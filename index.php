04bc8b8bdc810e9f2e1ceb4f25ead9c937a795b4
Thiago <mthiago299@gmail.com>
2022-03-26 11:25:11 -0300

    comit inicial

--- index.php
+++ index.php
@@ -1,6 +1,41 @@
 <?php
-echo 'Hello world';
+
+$categorias = []; 
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+
+//print_r($categorias);
+
+$nome = 'Thiago';
+$idade = '19';
 
 
+if ($idade >= 6 && $idade <= 12)
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+            echo "O nadador ".$nome. " compete na categoria infantil";
+    }
+}
+
+else if($idade >= 13 && $idade <= 18)
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+            echo "O nadador ".$nome. " compete na categoria adolescente";
+    }
+}
+
+else{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adulto')
+            echo "O nadador ".$nome. " compete na categoria adulto";
+    }
+}
 
 ?>
